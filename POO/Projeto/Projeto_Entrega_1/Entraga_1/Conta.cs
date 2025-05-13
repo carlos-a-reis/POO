e13805295c718fed11cfc16e10967313c152e644
Carlos Augusto dos Reis <hadqen@gmail.com>
2025-05-13 20:19:43 -0300

    Entrega 1 - inicio do projeto

---
+++ POO/Projeto/Projeto_Entrega_1/Entraga_1/Conta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Entrega_1.Entraga_1
+{
+    class Conta
+    {
+        private double Saldo { get { return Saldo; } set { }}
+        private int NumeroAgencia { get; set; }
+        private int NumeroConta { get; set; }
+        private string NomeTitular { get; set; }
+
+        public double getSaldo() { return Saldo;}
+        public int getNumeroAgenci() { return NumeroAgencia; }
+        public double getNumeroConta() { return NumeroConta; }
+        public string getNomeTitular() { return NomeTitular; }
+
+
+        public Conta(double saldo, int numeroAgencia, int numeroConta, string nomeTitular)
+        {
+            Saldo = saldo;
+            NumeroAgencia = numeroAgencia;
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+        }
+    }
+}
---
+++ POO/Projeto/Projeto_Entrega_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto_Entrega_1.Entraga_1;
+
+namespace Projeto_Entrega_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta(100.00, 123, 456, "carlos");
+
+            conta1.getSaldo();
+        }
+    }
+}
