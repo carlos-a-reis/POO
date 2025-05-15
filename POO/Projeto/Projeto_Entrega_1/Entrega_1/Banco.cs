5ff248fedf7c84f4ef2b9978f12ccd2c2e290d26
carlos <carlos.aug.dosreis@gamil.com>
2025-05-15 11:22:54 -0300

    Correção da pasta entrega, correção da Classe Conta e criação da classe Banco

---
+++ POO/Projeto/Projeto_Entrega_1/Entrega_1/Banco.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Entrega_1.Entrega_1
+{
+    class Banco
+    {
+    }
+}
--- POO/Projeto/Projeto_Entrega_1/Program.cs
+++ POO/Projeto/Projeto_Entrega_1/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Projeto_Entrega_1.Entraga_1;
+using Projeto_Entrega_1.Entrega_1;
 
 namespace Projeto_Entrega_1
 {
@@ -11,10 +11,10 @@ namespace Projeto_Entrega_1
     {
         static void Main(string[] args)
         {
-            Conta conta1 = new Conta(100.171, 123, 456, "carlos");
+            Conta conta1 = new Conta(100, 123, 456, "carlos");
 
             Console.WriteLine(conta1.GetSaldo());
-            conta1.Depositar(50.00);
+            conta1.Depositar(60.00);
             Console.WriteLine(conta1.GetSaldo());
         }
     }
